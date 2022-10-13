c14df66fdaacf985b19103f7d9c12943b4dc3222
Tushar K <kohli.tushar18@gmail.com>
2022-10-13 01:03:33 -0500

    version 1

---
+++ homebrew-yabai2.rb
@@ -0,0 +1,33 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class HomebrewYabai2 < Formula
+  desc "A tiling window manager for macOS based on binary space partitioning"
+  homepage "https://github.com/Tushar1729-k/homebrew-yabai2"
+  url "https://github.com/Tushar1729-k/homebrew-yabai2/archive/refs/tags/v1.tar.gz"
+  sha256 "1004977c73fb09901b0394de9f0a8989f6e5c88dcbf614edadb0b2457cf99c7c"
+  license "MIT"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test homebrew-yabai2`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
